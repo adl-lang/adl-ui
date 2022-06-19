3d43c226138669a751e6a4204870145ef46e1e7b
Tim Docker <tim@dockerz.net>
2022-06-19 18:30:39 +1000

    Improve comments

--- src/lib/fieldfns/type.ts
+++ src/lib/fieldfns/type.ts
@@ -1,16 +1,17 @@
-
-// A record specifying operations on a text field:
-//
-//    toText   : convert a js value to the text content
-//    validate : validate the text content returning a validate
-//               error message on failure, or null on success
-//    fromText : convert the text content back into a js value
-//    equals   : compare two js values for this field
-
+// A record mapping a value of type T to a text field
 export interface FieldFns<T> {
+
+  // Convert a value to the text content
   toText(value: T): string;
+
+  // validate the text content returning null on success
+  // or an error message on failure
   validate(text: string): null | string;
+
+  // assuming valid, convert the text content back to a value
   fromText(text: string): T;
+
+  // Returns true if two values are equals
   equals(v1: T, v2: T): boolean;
 }
 
